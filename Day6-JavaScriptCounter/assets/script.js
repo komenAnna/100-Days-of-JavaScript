cfe647749473d440a6235c1d41e4d51b7a80e7c1
komenAnna <komenriana@gmail.com>
2022-11-30 10:17:48 +0300

    Added some comments

--- Day6-JavaScriptCounter/assets/script.js
+++ Day6-JavaScriptCounter/assets/script.js
@@ -5,15 +5,18 @@
     // add event listener and a function to increase and decrease
     buttons.forEach(function(button){
         button.addEventListener("click",function(){
+            // if the specific button contains the "specified class", decrement or increment the count
             if(button.classList.contains("decrease-btn")){
                 count--
             } else if(button.classList.contains("increase-btn")){
                 count++
             }
 
+            // add the 'count' variable as textContent to the class 'counter-machine
             const counter = document.querySelector(".counter-machine")
             counter.textContent = count
 
+            //change the font color of the var 'counter' when it meets a certain condition
             if(count > 0){
                 counter.style.color = "green"
             } else if (count < 0){
